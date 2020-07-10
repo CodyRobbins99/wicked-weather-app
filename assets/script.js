871c1750d892330c6c008ae7ee8ae55741aabf3e
Coodie <65614373+CodyRobbins99@users.noreply.github.com>
2020-07-10 15:40:54 -0400

    5 day forecast implimented

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,8 @@ const temperatureEl = document.querySelector("#temperature-span");
 const humidityEl = document.querySelector("#humidity-span");
 const windSpeedEl = document.querySelector("#wind-speed-span");
 const uvIndexEl = document.querySelector("#uv-index-span");
+const weatherIconEl = document.querySelector("#weather-icon-span");
+const forecastContainerEl = document.querySelector("#forecast-container");
 
 const formSubmitHandler = function() {
     event.preventDefault();
@@ -14,7 +16,7 @@ const formSubmitHandler = function() {
 
     if (cityName) {
         getForecast(cityName);
-        cityNameEl.value = '';
+        cityInputEl.value = '';
     }
     else {
         alert("Please enter a valid city!");
@@ -45,6 +47,10 @@ const getForecast = function(city) {
         const dateObject = new Date(milliseconds)
         const humanDateFormat = dateObject.toLocaleString()
 
+        const icon = currentWeatherResponse.weather[0].icon
+        weatherIconEl.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" />`
+        weatherIconEl.setAttribute("class", "mw-25 mh-25")
+
         cityNameEl.textContent = currentWeatherResponse.name;
         dateEl.textContent = humanDateFormat
                 
@@ -70,6 +76,53 @@ const getForecast = function(city) {
         else if (uvIndexEl.textContent > 6.6) {
             uvIndexEl.setAttribute("class", "bg-danger  border border-danger rounded");
         }
+
+        for (var i = 0; i < data.daily.length - 3; i++) {
+            // create a div for forecast card
+            const forecastCardEl = document.createElement(`div`);
+            forecastCardEl.classList = `col-2 bg-info row p-1 border border-info rounded`;
+
+            // create a date 
+            const unixTimestamp = data.daily[i].dt;
+            const milliseconds = unixTimestamp * 1000
+            const dateObject = new Date(milliseconds)
+            const humanDateFormat = dateObject.toLocaleString()
+            const forecastCardDate = document.createElement(`h5`);
+            forecastCardDate.textContent = humanDateFormat;
+            forecastCardDate.classList = `text-uppercase col-12`
+
+            // append to container
+            forecastCardEl.appendChild(forecastCardDate);
+
+            // create an icon
+            const iconValue = data.daily[i].weather[0].icon
+            const forecastIconEl = document.createElement(`img`);
+            forecastIconEl.setAttribute(`src`,`http://openweathermap.org/img/wn/${iconValue}@2x.png`);
+            forecastIconEl.classList = `mw-25 mh-25 col-12`;
+
+            // append to container
+            forecastCardEl.appendChild(forecastIconEl);
+
+            // create a temp
+            const tempValue = data.daily[i].temp.day
+            const forecastTempEl = document.createElement(`p`);
+            forecastTempEl.textContent = `${tempValue}°F`
+            forecastTempEl.classList = `col-12 text-uppercase`
+
+            // append to container
+            forecastCardEl.appendChild(forecastTempEl);
+
+            // create a humidity
+            const humidtyValue = data.daily[i].humidity
+            const forecastHumidityEl = document.createElement(`p`);
+            forecastHumidityEl.textContent = `${humidtyValue}% Humidity`
+            forecastHumidityEl.classList = `col-12`
+
+            // append to container
+            forecastCardEl.appendChild(forecastHumidityEl);
+
+            forecastContainerEl.appendChild(forecastCardEl);
+        }        
     })
 };
 
